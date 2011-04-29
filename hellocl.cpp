26fbcf0f13f9f2a2642c1c70c67cf5043ba3d404
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-04-29 20:52:32 +0200

    image copying with buffers works

--- hellocl.cpp
+++ hellocl.cpp
@@ -22,7 +22,7 @@ void init_cl(cl::Context& context,
     }
     cl_context_properties properties[] =
         { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0 };
-    context = cl::Context(CL_DEVICE_TYPE_GPU, properties);
+    context = cl::Context(CL_DEVICE_TYPE_CPU, properties);
     devices = std::vector<cl::Device>(context.getInfo<CL_CONTEXT_DEVICES>());
 
     queue = cl::CommandQueue(context, devices[0], 0, &err);
@@ -49,17 +49,24 @@ void init_cl(cl::Context& context,
 
 std::string helloStr = R"(
 
-__kernel void hello(__read_only image2d_t in,
-                    __write_only image2d_t out) {
-  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE |
-                            CLK_FILTER_NEAREST |
-                            CLK_ADDRESS_CLAMP;
+__kernel void hello(__global uchar* in,
+                    __global uchar* out) {
+  // const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE |
+  //                           CLK_FILTER_NEAREST |
+  //                           CLK_ADDRESS_CLAMP;
 
-  int gid0 = get_global_id(0);
-  int gid1 = get_global_id(1);
-  uint4 pixel;
-  pixel = read_imageui(in, sampler, (int2)(gid0, gid1));
-  write_imageui(out, (int2)(gid0, gid1), (uint4)(0));
+  // int x = get_global_id(0);
+  // int y = get_global_id(1);
+  int i = get_global_id(0);
+  out[i] = in[i];
+  // uint4 pixel;
+  // pixel = read_imageui(in, sampler, (int2)(x, y));
+  // pixel.s3 = 255;
+  // write_imageui(out, (int2)(x, y), pixel);
+  // out[y * 300 * 4 + x * 4] = 0;
+  // out[y * 300 * 4 + x * 4 + 1] = 0;
+  // out[y * 300 * 4 + x * 4 + 2] = 0;
+  // out[y * 300 * 4 + x * 4 + 3] = 255;
 
   // write_imageui(out, (int2)(x, y), (uint4)(0, 0, 0, 0));
   // uint4 pixel = read_imageui(in, sampler, (int2)(x, y));
@@ -93,28 +100,28 @@ int main() {
 
   // Load image
   cv::Mat image = cv::imread("RGB.png");
-  cv::Mat with_alpha_pre = add_alpha_channel(image);
-  cv::Mat with_alpha = with_alpha_pre.clone();
+  cv::Mat with_alpha = add_alpha_channel(image).clone();
+  std::cerr << image.rows << " " << image.cols << std::endl;
 
-  cl::Image2D cl_img_i(context, CL_MEM_READ_ONLY,
-                       cl::ImageFormat(CL_BGRA, CL_UNORM_INT8),
-                       image.rows, image.cols);
-  cl::Image2D cl_img_o(context, CL_MEM_WRITE_ONLY,
-                       cl::ImageFormat(CL_BGRA, CL_UNORM_INT8),
-                       image.rows, image.cols);
-  // cl::Buffer buffer_in (context, CL_MEM_READ_ONLY,  SIZE * sizeof(float));
-  // cl::Buffer buffer_out(context, CL_MEM_WRITE_ONLY, SIZE * sizeof(float));
-  // queue.enqueueWriteBuffer(buffer_in, CL_TRUE, 0,
-  //                          SIZE * sizeof(float), &in[0]);
+  // cl::Image2D cl_img_i(context, CL_MEM_READ_ONLY,
+  //                      cl::ImageFormat(CL_BGRA, CL_UNORM_INT8),
+  //                      image.rows, image.cols);
+  // cl::Image2D cl_img_o(context, CL_MEM_WRITE_ONLY,
+  //                      cl::ImageFormat(CL_BGRA, CL_UNORM_INT8),
+  //                      image.rows, image.cols);
+  cl::Buffer buffer_in (context, CL_MEM_READ_ONLY,  image.rows * image.cols * 4);
+  cl::Buffer buffer_out(context, CL_MEM_WRITE_ONLY, image.rows * image.cols * 4);
+  queue.enqueueWriteBuffer(buffer_in, CL_TRUE, 0,
+                           image.rows * image.cols * 4, with_alpha.data);
 
-  cl::size_t<3> origin;
-  origin.push_back(0);
-  origin.push_back(0);
-  origin.push_back(0);
-  cl::size_t<3> region;
-  region.push_back(image.rows);
-  region.push_back(image.cols);
-  region.push_back(1);
+  // cl::size_t<3> origin;
+  // origin.push_back(0);
+  // origin.push_back(0);
+  // origin.push_back(0);
+  // cl::size_t<3> region;
+  // region.push_back(image.rows);
+  // region.push_back(image.cols);
+  // region.push_back(1);
   // unsigned char test[300*300*4];
   // for (int i = 0; i < 300*300*4; ++i) {
   //   test[i] = -1;
@@ -122,31 +129,28 @@ int main() {
   //   //   test[i] = 255;
   //   // }
   // }
-  queue.enqueueWriteImage(cl_img_i, CL_TRUE,
-                          origin, region, 0, 0,
-                          with_alpha.data);
-  queue.enqueueWriteImage(cl_img_o, CL_TRUE,
-                          origin, region, 0, 0,
-                          with_alpha.data);
+  // queue.enqueueWriteImage(cl_img_i, CL_TRUE,
+  //                         origin, region, 0, 0,
+  //                         with_alpha.data);
 
   cl::Kernel kernel(program, "hello", &err);
-  kernel.setArg<cl::Image2D>(0, cl_img_i);
-  kernel.setArg<cl::Image2D>(1, cl_img_o);
+  kernel.setArg<cl::Buffer>(0, buffer_in);
+  kernel.setArg<cl::Buffer>(1, buffer_out);
 
   cl::Event event;
   queue.enqueueNDRangeKernel(
       kernel,
       cl::NullRange,
-      cl::NDRange(image.rows, image.cols),
+      cl::NDRange(image.rows * image.cols * 4),
       cl::NullRange,
       NULL,
       &event);
   event.wait();
 
   cv::Mat out_mat(image.size(), CV_8UC4);
-  queue.enqueueReadImage(cl_img_o, CL_TRUE,
-                         origin, region, 0, 0,
-                         out_mat.data);
+  queue.enqueueReadBuffer(buffer_out, CL_TRUE, 0,
+                          image.rows * image.cols * 4,
+                          out_mat.data);
 
   // Write result image
   cv::imwrite("1.png", out_mat);
