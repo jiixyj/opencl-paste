ce6f9534de60842af38a9beffbbaa622a7264837
Jan Kokemüller <jan.kokemueller@gmail.com>
2011-05-01 00:43:43 +0200

    remove stdcl

--- hellocl.cpp
+++ hellocl.cpp
@@ -1,13 +1,52 @@
-#include <stdcl.h>
-
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
 
 #include <array>
+#include <fstream>
 #include <iostream>
 
 #include <cv.h>
 #include <highgui.h>
+
+void init_cl(cl::Context& context,
+             cl::Program& program,
+             cl::CommandQueue& queue,
+             std::string const& src) {
+  cl_int err = CL_SUCCESS;
+  std::vector<cl::Device> devices;
+  try {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if (platforms.size() == 0) {
+      std::cerr << "Platform size 0" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    cl_context_properties properties[] =
+        { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0 };
+    context = cl::Context(CL_DEVICE_TYPE_CPU, properties);
+    devices = std::vector<cl::Device>(context.getInfo<CL_CONTEXT_DEVICES>());
+
+    queue = cl::CommandQueue(context, devices[0], 0, &err);
+
+    cl::Program::Sources source {
+      std::make_pair(src.c_str(), src.size())
+    };
+    program = cl::Program(context, source);
+    program.build(devices);
+    std::string log;
+    program.getBuildInfo<std::string>(devices[0], CL_PROGRAM_BUILD_LOG, &log);
+    std::cerr << log;
+  } catch (cl::Error err) {
+    std::cerr << "ERROR: "
+              << err.what()
+              << "(" << err.err() << ")"
+              << std::endl;
+    std::string log;
+    program.getBuildInfo<std::string>(devices[0], CL_PROGRAM_BUILD_LOG, &log);
+    std::cerr << log;
+    exit(EXIT_FAILURE);
+  }
+}
 
 cv::Mat make_rgba(const cv::Mat& image) {
   static int fromto[] = {0, 2,  1, 1,  2, 0,  3, 3};
@@ -22,13 +61,15 @@ int main() {
   cv::Mat image = cv::imread("RGB.png");
   cv::Mat with_alpha = make_rgba(image);
 
-  CONTEXT* cp = (stdgpu) ? stdgpu : stdcpu;
-  cl::Context context(cp->ctx);
-  cl::CommandQueue queue(cp->cmdq[0]);
+  std::ifstream ifs("hellocl_kernels.cl");
+  std::string hellocl_kernels((std::istreambuf_iterator<char>(ifs)),
+                               std::istreambuf_iterator<char>());
 
-  void* clh = clopen(cp, "hellocl_kernels.cl", CLLD_NOW);
-  cl_kernel kernel_cl = clsym(cp, clh, "hello", CLLD_NOW);
-  cl::Kernel kernel(kernel_cl);
+  cl::Context context;
+  cl::Program program;
+  cl::CommandQueue queue;
+  init_cl(context, program, queue, hellocl_kernels);
+
 
   cl::Image2D cl_img_i(context, CL_MEM_READ_ONLY,
                        cl::ImageFormat(CL_RGBA, CL_UNSIGNED_INT8),
@@ -49,6 +90,7 @@ int main() {
                           origin, region, 0, 0,
                           with_alpha.data);
 
+  cl::Kernel kernel(program, "hello", NULL);
   kernel.setArg<cl::Image2D>(0, cl_img_i);
   kernel.setArg<cl::Image2D>(1, cl_img_o);
   kernel.setArg<cl_int>(2, 0);
